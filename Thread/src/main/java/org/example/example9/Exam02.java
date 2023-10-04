7fde42c53ca23ecd0af218f874c83526f1d8ca43
jeongwoolee <sun62073812@gmail.com>
2023-10-04 20:50:31 +0900

    CODE : DAEMON

---
+++ Thread/src/main/java/org/example/example9/Exam02.java
@@ -0,0 +1,15 @@
+package org.example.example9;
+
+public class Exam02 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Worker worker = new Worker("worker1", 100);
+
+        worker.start();
+
+        Thread.sleep(5000);
+        worker.stop();
+
+        System.out.println("Main Thread terminated");
+    }
+}
